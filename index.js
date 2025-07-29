1313c626b03677657f9fb6a3c2c741b7967c26e8
Daniel Bohlke <thatkidthere1997@gmail.com>
2025-07-28 20:19:54 -0700

    Forgot to update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
-const { Player } = require('./Player.js');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
